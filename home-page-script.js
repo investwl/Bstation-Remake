8caa9a8bb9cf3eba6629abbcf77750b0997482b8
investwl <williangjym@gmail.com>
2024-04-22 00:21:16 +0700

    Updated Home Page
    still unable to add hyperlink to the image cards, updated image sources for each image tag, added hyperlink for genre marquee banner thingy, the show more button still have to be added JS.

--- home-page-script.js
+++ home-page-script.js
@@ -1,11 +1,24 @@
 
 let slideIndex = 0;
+let mainSlideIndex = 0;
+showMainSlides();
 showSlides();
+
+function showMainSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mainslide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  mainSlideIndex++;
+  if (mainSlideIndex > slides.length) {mainSlideIndex = 1}    
+  slides[mainSlideIndex-1].style.display = "block";  
+  setTimeout(showMainSlides, 3000);
+}
 
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
