180070f2a9854853997d2761624ad84379ad57e9
Crimson-Squadron <mcindo138@gmail.com>
2024-04-24 21:19:45 +0700

    Add expandable video recommendation to home

--- home-page-script.js
+++ home-page-script.js
@@ -53,3 +53,22 @@ navigate.forEach(function(div) {
     window.location.href = 'video-page.html'
   })
 })
+
+function toggleRec() {
+  var rec = document.querySelector('.recommend-vids')
+  var expand = document.querySelector('.rec-show-more')
+  var txt = expand.querySelector('a')
+  var img = expand.querySelector('img')
+  rec.classList.toggle('more')
+  if (rec.classList.contains('more')) {
+      rec.style.height = 'auto'
+      rec.style.paddingBottom = '45px'
+      txt.textContent = 'Show less'
+      img.src = "images/chevron-up.svg"
+  } else {
+      rec.style.height = '480px'
+      rec.style.paddingBottom = '0px'
+      txt.textContent = 'Show more'
+      img.src = "images/chevron-down.svg"
+  }
+}
